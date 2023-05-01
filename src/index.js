18cbd7cec563760422f6f30404cb588a36deca3d
AdriiiCamacho <adricam@uma.es>
2023-05-01 21:01:37 +0200

    deploy18

--- src/index.js
+++ src/index.js
@@ -13,9 +13,11 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App/>
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
