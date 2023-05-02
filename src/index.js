6d2e83be4c0dd44af6779c1dc8c093bb7ea384ea
AdriiiCamacho <adricam@uma.es>
2023-05-02 13:03:16 +0200

    deploy24

--- src/index.js
+++ src/index.js
@@ -4,20 +4,18 @@ import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 import {
-  BrowserRouter,
-  BrowserRouter as Router,
-  Route
+  HashRouter
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
   <React.StrictMode>
+  <HashRouter>
     <App/>
+  </HashRouter>
   </React.StrictMode>
-  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
